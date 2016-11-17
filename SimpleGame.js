1dbf530d9a0e182e5465fb31c016cea95d62e3bb
Colby Green <cjdude22@gmail.com>
2016-11-17 08:52:57 -0800

    wabbalabbadubdub

--- SimpleGame.js
+++ SimpleGame.js
@@ -3,26 +3,26 @@ var yval = 100;
 
 document.addEventListener("keydown", function(e) {
   if (e.keyCode == 37) {
- xval -= 15;
+ xval -= 30;
        var move = document.getElementById("van");
   move.setAttribute("x", xval)
   }
 
   if (e.keyCode == 39) {
-    xval += 15;
+    xval += 30;
        var move = document.getElementById("van");
   move.setAttribute("x", xval)
   }
 })
 document.addEventListener("keydown", function(e) {
   if (e.keyCode == 38) {
- yval -= 15;
+ yval -= 30;
        var move = document.getElementById("van");
   move.setAttribute("y", yval)
   }
 
   if (e.keyCode == 40) {
-    yval += 15;
+    yval += 30;
        var move = document.getElementById("van");
   move.setAttribute("y", yval)
   }
