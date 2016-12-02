a2765aaf0d639f5b2b8a28316fff44465415ae61
Colby Green <cjdude22@gmail.com>
2016-12-02 08:50:01 -0800

    End Of Game

--- SimpleGame.js
+++ SimpleGame.js
@@ -1,17 +1,19 @@
-
 var xval = 100;
 var yval = 100;
 var foodEaten = 0;
-
+var timeStart = Date.now();
 /*Key Down Event*/
 document.addEventListener("keydown", function(e) {
 var xval = Number(document.getElementById("van").getAttribute("x"));
 var yval = Number(document.getElementById("van").getAttribute("y"));
 var snackX = Number(document.getElementById("snack1").getAttribute("x"));
 var snackY = Number(document.getElementById("snack1").getAttribute("y"));
+var snackX2 = Number(document.getElementById("snack2").getAttribute("x"));
+var snackY2 = Number(document.getElementById("snack2").getAttribute("y"));
 
-var random = Math.floor((Math.random() * 650) + 0);
-var random1 = Math.floor((Math.random() * 1000) + 0);
+var random = Math.floor((Math.random() * 1000) + 0);
+var random2 = Math.floor((Math.random() * 1000) + 0);
+
 /*Left Movement */
   if (e.keyCode == 37) {
  xval -= 30;
@@ -38,7 +40,26 @@ var random1 = Math.floor((Math.random() * 1000) + 0);
 /*Overlap*/
   if(xval >= snackX-75 && xval <= snackX+75 && yval >= snackY-75 && yval <= snackY+75 ){
     console.log(foodEaten);
-    document.getElementById("snack1").setAttribute("x", random1)
+    document.getElementById("snack1").setAttribute("x", random);
     foodEaten += 1;
+    document.getElementById("score").textContent = foodEaten;
+  }
+  /*Overlap 2*/
+  if(xval >= snackX2-75 && xval <= snackX2+75 && yval >= snackY2-75 && yval <= snackY2+75 ){
+    console.log(foodEaten);
+    document.getElementById("snack2").setAttribute("x", random2);
+    foodEaten += 1;
+    document.getElementById("score").textContent = foodEaten;
+  }
+/*End of Game*/
+  if(foodEaten == 10){
+    document.getElementById("game").pauseAnimations();
+    var timeStop = Date.now();
+    var theend = timeStop - timeStart;
+    var endofgame = Math.round(theend/1000);
+    document.getElementById("end").textContent = endofgame;
+    document.getElementById("seconds").setAttribute("opacity", 1);
+    document.getElementById("box").setAttribute("opacity", 1);
+    document.getElementById("finishedin").setAttribute("opacity", 1);
   }
 })
